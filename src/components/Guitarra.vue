f743db2b96473c86292482992af11839d226d0fb
DubanPrograDVR <156736859+DubanPrograDVR@users.noreply.github.com>
2025-09-10 12:56:07 -0300

    Base de Datos local y iteracion de productos

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  guitarra: {
+    type: Object,
+    required: true,
+  },
+});
+
+const numero = ref(0);
+
+const incrementar = () => {
+  alert("Diste click");
+};
+</script>
+
+<template>
+  <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+      <img
+        class="img-fluid"
+        v-bind:src="'/img/' + guitarra.imagen + '.jpg'"
+        :alt="'imagen guitarra' + guitarra.nombre" />
+    </div>
+    <div class="col-8">
+      <h3 class="text-black fs-4 fw-bold text-uppercase">
+        {{ guitarra.nombre }}
+      </h3>
+      <p>{{ numero }}</p>
+      <p>{{ guitarra.descripcion }}</p>
+      <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+      <button type="button" v-on:click="incrementar" class="btn btn-dark w-100">
+        Agregar al Carrito
+      </button>
+    </div>
+  </div>
+</template>
